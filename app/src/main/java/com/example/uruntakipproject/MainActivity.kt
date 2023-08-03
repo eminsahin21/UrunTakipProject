a1bb5b4575618253bd4875613083dddcfd86108b
Mehmet Emin <mehmeteminsahin34@gmail.com>
2023-08-03 14:34:34 +0300

    qr kod çalışıyor

--- app/src/main/java/com/example/uruntakipproject/MainActivity.kt
+++ app/src/main/java/com/example/uruntakipproject/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.uruntakipproject
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.uruntakipproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    companion object{
+        const val RESULT = "RESULT"
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btnTara.setOnClickListener {
+            val intent = Intent(applicationContext , ScanActivity::class.java)
+            startActivity(intent)
+        }
+
+        val result = intent.getStringExtra(RESULT)
+
+        if (result != null){
+            if (result.contains("https://") || result.contains("http://")){
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(result))
+                startActivity(intent)
+            }
+            else{
+                binding.qrCodeText.text = result.toString()
+            }
+
+        }
+
+
     }
 
 }
