a79e1c9bb38bdba56c9f7e91579ecea485031d41
farukeynalli <afarukeynalli@gmail.com>
2023-08-07 13:21:20 +0300

    user and database :)

--- app/src/main/java/com/example/uruntakipproject/DatabaseHandler.kt
+++ app/src/main/java/com/example/uruntakipproject/DatabaseHandler.kt
@@ -1,22 +1,54 @@
 package com.example.uruntakipproject
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
 
-val DATABASE_NAME="MyDB"
-val TABLE_NAME = "deneme"
-val COL_NAME ="personelName"
-val COL_BIRIM ="birimi"
-val COL_ID ="id"
-val COL_CODENO ="code_no"
+val DATABASE_NAME="son.db"
+val TABLE_NAME = "ketıl"
+val COL_NAME ="NAME"
+val COL_BIRIM ="BIRIM"
+val COL_ID ="ID"
+val COL_CODENO ="CODE_NO"
 
 class DatabaseHandler(context:Context): SQLiteOpenHelper(context, DATABASE_NAME,null,1){
     override fun onCreate(p0: SQLiteDatabase?) {
-
+        p0?.execSQL("CREATE TABLE IF NOT EXISTS $TABLE_NAME(ID INTEGER PRIMARY KEY AUTOINCREMENT,NAME TEXT,BIRIM TEXT, CODE_NO TEXT)")
     }
 
     override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
     }
+
+    fun insertData(name:String,birimi:String,code_no:String): Boolean? {
+        val p0 =this.writableDatabase
+        var cv =ContentValues()
+        cv.put(COL_NAME,name)
+        cv.put(COL_BIRIM,birimi)
+        cv.put(COL_CODENO,code_no)
+
+        var res =p0.insert(TABLE_NAME,null,cv)
+
+        return !res.equals(-1)
+
+
+
+    }
+
+
+    fun getAllData():Cursor{
+        val p0 = this.writableDatabase
+        return p0.rawQuery("SELECT * FROM $TABLE_NAME",null)
+    }
+
+    fun getData(id:String):Cursor{
+        val p0 =this.writableDatabase
+        return p0.rawQuery("SELECT * FROM $TABLE_NAME WHERE ID=? ", arrayOf(id),null)
+
+    }
+
 
 }
--- app/src/main/java/com/example/uruntakipproject/MainActivity.kt
+++ app/src/main/java/com/example/uruntakipproject/MainActivity.kt
@@ -4,13 +4,18 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.uruntakipproject.databinding.ActivityMainBinding
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    lateinit var myDb:DatabaseHandler
 
-    companion object{
+    companion object {
         const val RESULT = "RESULT"
     }
 
@@ -19,28 +24,98 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        myDb = DatabaseHandler(this)
 
 
-        binding.btnTara.setOnClickListener {
-            val intent = Intent(applicationContext , ScanActivity::class.java)
-            startActivity(intent)
+        AddData()
+//        getData()
+
+        val res = myDb.getAllData()
+
+        if(res.getCount()==0){
+            showMessage("error ", "Nothing Found")
+//            return@OnClickListener
+        }
+        else{
+            val buffer =StringBuffer()
+            while (res.moveToNext()){
+                buffer.append("Id: " + res.getString(0)+"\n")
+                buffer.append("Name: " + res.getString(1)+"\n\n")
+                buffer.append("Birimi: " + res.getString(2)+"\n\n")
+                buffer.append("Code: " + res.getString(3)+"\n\n")
+            }
+//                showMessage("Data",buffer.toString())
+//            println("mehmet emşin")
         }
 
-        val result = intent.getStringExtra(RESULT)
 
-        if (result != null){
-            if (result.contains("https://") || result.contains("http://")){
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(result))
+
+            binding.btnTara.setOnClickListener {
+                val intent = Intent(applicationContext, ScanActivity::class.java)
                 startActivity(intent)
             }
-            else{
-                binding.qrCodeText.text = result.toString()
+
+            val result = intent.getStringExtra(RESULT)
+
+            if (result != null) {
+                if (result.contains("https://") || result.contains("http://")) {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(result))
+                    startActivity(intent)
+                } else {
+                    binding.qrCodeText.text = result.toString()
+
+                }
 
             }
 
+
         }
+
+    private fun getData() {
+        binding.getData.setOnClickListener(View.OnClickListener {
+
+
+                
+                
+        })
+    }
+
+    private fun showMessage(title: String, message: String) {
+        val builder =AlertDialog.Builder(this)
+        builder.create()
+        builder.setCancelable(true)
+        builder.setTitle(title)
+        builder.setMessage(message)
+        builder.show()
+    }
+
+    private fun AddData() {
+        binding.btninsert.setOnClickListener(View.OnClickListener setOnClickListener@{
+            val name = binding.nameText.text.toString().trim()
+            val birim = binding.birimText.text.toString().trim()
+            val codeNo = binding.codeText.text.toString().trim()
+
+            if (TextUtils.isEmpty(name)){
+                binding.nameText.error="Enter Name"
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(birim)){
+                binding.nameText.error="Enter birim no"
+                return@setOnClickListener
+            }
+            if (TextUtils.isEmpty(codeNo)){
+                binding.nameText.error="Enter codeNo"
+                return@setOnClickListener
+            }
+            val isInserted =myDb.insertData(name,birim,codeNo)
+            if (isInserted==true){
+                Toast.makeText(this, "Data inserted", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Data eklenemedi", Toast.LENGTH_SHORT).show()
+            }
+
+        })
 
 
     }
-
 }
