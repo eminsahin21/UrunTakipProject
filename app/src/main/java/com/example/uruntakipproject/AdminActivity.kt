c4bfa3e433e971e80bee59b695e6760cbd8785a6
Mehmet Emin <mehmeteminsahin34@gmail.com>
2023-08-23 15:31:04 +0300

    AdminActivity.kt düzenlendi

--- app/src/main/java/com/example/uruntakipproject/AdminActivity.kt
+++ app/src/main/java/com/example/uruntakipproject/AdminActivity.kt
@@ -4,10 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 
@@ -19,6 +16,13 @@ class AdminActivity : AppCompatActivity() {
     private lateinit var codeText: EditText
     private lateinit var buttonEkle: Button
     private lateinit var eklenenKayitText: TextView
+
+    private lateinit var bantA_cbox : CheckBox
+    private lateinit var bantB_cbox : CheckBox
+    private lateinit var depo_cbox : CheckBox
+    private lateinit var kontrol_cbox : CheckBox
+    private lateinit var admin_cbox : CheckBox
+
     lateinit var myDb:DatabaseHandler
     private lateinit var dbRef: DatabaseReference
 
@@ -30,15 +34,24 @@ class AdminActivity : AppCompatActivity() {
 
 
         nameText = findViewById(R.id.nameText)
-        birimText = findViewById(R.id.birimText)
         codeText = findViewById(R.id.codeText)
         buttonEkle = findViewById(R.id.button_ekle)
         eklenenKayitText = findViewById(R.id.eklenenKayitText)
 
 
-        dbRef = FirebaseDatabase.getInstance().getReference("Personel");
+        bantA_cbox = findViewById(R.id.checkBox1)
+        bantB_cbox = findViewById(R.id.checkBox2)
+        depo_cbox = findViewById(R.id.checkBox3)
+        kontrol_cbox = findViewById(R.id.checkBox4)
+        admin_cbox = findViewById(R.id.checkBox5)
 
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Personel");
         AddData()
+
+
+
+
 
     }
 
@@ -48,21 +61,40 @@ class AdminActivity : AppCompatActivity() {
             buttonEkle.setOnClickListener(View.OnClickListener setOnClickListener@{
 
                 val name = nameText.text.toString().trim()
-                val birim = birimText.text.toString().trim()
                 val codeNo = codeText.text.toString().trim()
 
                 if (TextUtils.isEmpty(name)){
                     nameText.error="Enter Name"
                     return@setOnClickListener
                 }
-                if (TextUtils.isEmpty(birim)){
-                    birimText.error="Enter birim no"
-                    return@setOnClickListener
-                }
                 if (TextUtils.isEmpty(codeNo)){
                     codeText.error="Enter codeNo"
                     return@setOnClickListener
                 }
+
+
+                var birim = ""
+
+                if (bantA_cbox.isChecked){
+                    birim += "1,"
+                }
+                if (bantB_cbox.isChecked){
+                    birim += "2,"
+                }
+                if (depo_cbox.isChecked){
+                    birim += "3,"
+                }
+                if (kontrol_cbox.isChecked){
+                    birim += "4,"
+                }
+                if (admin_cbox.isChecked){
+                    birim += "5,"
+                }
+
+
+                println("Birimi:{$birim}")
+
+
                 val perId =dbRef.push().key!!
                 val personel = PersonelModel(perId,name,birim,codeNo)
 
@@ -79,7 +111,6 @@ class AdminActivity : AppCompatActivity() {
                 if (isInserted==true){
                     nameText.text.clear()
                     codeText.text.clear()
-                    birimText.text.clear()
                     Toast.makeText(this, "Data inserted", Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(this, "Data eklenemedi", Toast.LENGTH_SHORT).show()
